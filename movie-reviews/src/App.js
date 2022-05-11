d7a4c2fa8918c37869bf9a6af16b746d148c22f0
robinlaws <94911535+robinlaws@users.noreply.github.com>
2022-05-11 13:45:19 -0230

    test
    test

--- movie-reviews/src/App.js
+++ movie-reviews/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+function Header(props) {
+  return(
+    <header>
+      <h1>Movie Reviews</h1>
+    </header>
+  );
+  }
+
+  function Main() {
+    return(
+      <section>
+        <h2>Movie reviews here</h2>
+      </section>
+    );
+  }
+
+  function Footer(){
+    return (
+      <footer>
+      <p>Footer</p>
+      </footer>
+    );
+  }
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
