8c4cbed8e6fd13a1f529507b1cc3f325b004f334
robinlaws <94911535+robinlaws@users.noreply.github.com>
2022-05-11 15:21:57 -0230

    update
    update

--- movie-reviews/src/App.js
+++ movie-reviews/src/App.js
@@ -4,33 +4,47 @@ import './App.css';
 function Header(props) {
   return(
     <header>
+      <div className="topnav">
+        <a className="active" href="reviews">Movie Reviews</a>
+        <a href="user_review">Leave Review</a>
+      </div>
       <h1>Movie Reviews</h1>
     </header>
   );
   }
 
-  function Main() {
+  function Main(props) {
     return(
       <section>
-        <h2>Movie reviews here</h2>
+        <ul style={{textAlign: "left"}}>
+        {props.movies.map(movie => <li key={movie.id}>{movie.title}</li>)}
+        </ul>
       </section>
     );
   }
 
-  function Footer(){
+  function Footer(props){
     return (
       <footer>
-      <p>Footer</p>
+      <p>Copyright {props.year}</p>
       </footer>
     );
   }
+
+
+const movies = [
+  "Titanic", "Lion King", "Gone With the Wind"
+];
+
+//variables
+const movieObjects = movies.map((movie, i) => ({id: i, title: movie}));
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Main />
-      <Footer />
+      <Main movies = {movieObjects}/>
+      <Footer year={new Date().getFullYear()} />
     </div>
   );
 }
