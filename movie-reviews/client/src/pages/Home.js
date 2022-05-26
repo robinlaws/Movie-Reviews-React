4d4e768a594745c84c912fa734f0fdd45ba1e0a4
robinlaws <94911535+robinlaws@users.noreply.github.com>
2022-05-25 22:26:01 -0230

    update
    update

--- movie-reviews/client/src/pages/Home.js
+++ movie-reviews/client/src/pages/Home.js
@@ -5,8 +5,6 @@ import Axios from "axios";
 
 
 export function Home(props){
-    props.getMovies();
-
     const removeMovie = async (event) => {
         const confirm = window.confirm("Are you sure you want to remove " + event.target.name + "?");
         if(confirm){
@@ -18,28 +16,29 @@ export function Home(props){
        Axios.post(url, {title: data}, options).then ((response) => {
         console.log(response);
        });
-       await props.getMovies();
        alert("Movie successfully deleted!")};
+       await props.getMovies();      
     };
+
     
     if((props.movies).length > 0){
         return (
             <div className="container">
-            <div style={{paddingBottom: "100px"}} id="align-content-*-center">
-                <h1><strong>MOVIE REVIEWS</strong></h1>
-                <hr></hr>
-                <ul style={{listStyleType: "none", textAlign: "center"  }}>
-                {props.movies.map((movie)=> ( 
-                    <>
-                    <li key={movie.index}><h4><strong>{movie.title}</strong></h4></li>
-                    <li key={movie.index}>Release: {movie.release}</li>
-                    <li key={movie.index}>Actors: {movie.actors}</li>
-                    <li key={movie.index}>Rating: {movie.rating}/5</li>
-                    <img key={movie.index} src={"./movieImages/" + movie.image} alt="movie poster"></img><br /><br />
-                    <button className= "btn-primary" onClick={removeMovie} name={movie.title}>Remove Movie</button>               
+                <div style={{paddingBottom: "100px"}} id="align-content-*-center">
+                    <h1><strong>MOVIE REVIEWS</strong></h1>
                     <hr></hr>
-                    </>
-                ))}
+                    <ul style={{listStyleType: "none", textAlign: "center"  }}>
+                    {props.movies.map((movie)=> ( 
+                        <>
+                            <li><h4><strong>{movie.title}</strong></h4></li>
+                            <li>Release: {movie.release}</li>
+                            <li>Actors: {movie.actors}</li>
+                            <li>Rating: {movie.rating}/5</li>
+                            <img src={"./movieImages/" + movie.image} alt="movie poster"></img><br /><br />
+                            <button className= "btn-primary" onClick={removeMovie} name={movie.title}>Remove Movie</button>               
+                            <hr></hr>
+                        </>
+                    ))}
                 </ul>
             </div>
         </div>
